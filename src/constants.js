465617fd4a5f893c95df6b48a977529d089491e0
i8023chu <58382197+i8023chu@users.noreply.github.com>
2026-06-26 09:39:41 +0800

    Update application constants for new project

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 // Application constants
-export const APP_NAME = 'Sublink Worker';
+export const APP_NAME = 'i8023chu's sub-converter';
 export const APP_VERSION = '2.4.2';
-export const GITHUB_REPO = 'https://github.com/7Sageer/sublink-worker';
+export const GITHUB_REPO = 'https://github.com/i8023chu/sub-converter';
 export const GITHUB_API_RELEASES = 'https://api.github.com/repos/7Sageer/sublink-worker/releases/latest';
 export const DOCS_URL = 'https://sublink.works';
 
